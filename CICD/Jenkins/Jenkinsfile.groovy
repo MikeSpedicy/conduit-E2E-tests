c51925a5c4096fc9d1eab6aaba88b34832f55915
Mykhailo K <mikespedicy@gmail.com>
2023-12-13 20:29:41 +0200

    git webhook test 3

--- CICD/Jenkins/Jenkinsfile.groovy
+++ CICD/Jenkins/Jenkinsfile.groovy
@@ -1,6 +1,6 @@
 pipeline {
     agent any
-    // fake changes
+    // fake changes 2
     // environment {
     //     astraUsername = credentials("${astraUsername}")
     //     astraPassword = credentials("${astraPassword}")
